a93fa96b3c2ea2c6aab4c39dfd6eb460b0d068cd
Sai Mangipudi <thegeekskm101@gmail.com>
2024-10-06 11:11:57 -0500

    Coordinate System and Windows Revamp

--- Assets/_GAME/Scripts/CoordinateSystem/CoordinateManager.cs
+++ Assets/_GAME/Scripts/CoordinateSystem/CoordinateManager.cs
@@ -1,10 +1,15 @@
 using SaiUtils;
 using SaiUtils.GameEvents;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CoordinateManager : MonoBehaviour
 {
     [SerializeField] private CoordinateSpawn _coordinate;
+    [SerializeField] bool _useScreenCoordinateSpawn = true;
+    [SerializeField] TransformEvent _screenCoordinateEvent;
+    [SerializeField] Transform _coordinateParent;
+    [SerializeField] VoidEvent _coordinateDisableEvent;
 
     void Update()
     {
@@ -13,11 +18,28 @@ public class CoordinateManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
-
-            _coordinate.transform.position = worldPosition;
-            _coordinate.Initialize();
+            if (!_useScreenCoordinateSpawn) HandleWorldCoordinateSpawn();
+            else HandleScreenCoordinateSpawn();
         }
     }
+
+    void HandleWorldCoordinateSpawn()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+
+        _coordinate.transform.position = worldPosition;
+        _coordinate.Initialize();
+    }
+
+    void HandleScreenCoordinateSpawn()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+        _coordinateParent.position = new Vector3(worldPosition.x, 0, worldPosition.z);
+
+        _screenCoordinateEvent?.Raise(_coordinateParent);
+    }
+
+    public void DisableCoordinate() => _coordinateDisableEvent?.Raise();
 }
---
+++ Assets/_GAME/Scripts/CoordinateSystem/ScreenCoordinateDisplay.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenCoordinateDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _text;
+    [SerializeField] Image _image;
+    [SerializeField] RectTransform _rectTransform;
+    [SerializeField] float _displayTime = 10f;
+    [SerializeField] Vector2 _offScreenAnchoredPosition;
+
+    Transform _coordinateParent;
+
+
+    void Awake()
+    {
+        if (!_rectTransform) _rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        _rectTransform.anchoredPosition = _offScreenAnchoredPosition;
+        _coordinateParent = null;
+    }
+    public void Initialize(Transform coordinateParent)
+    {
+        _coordinateParent = coordinateParent;
+        _text.text = $"[{Mathf.RoundToInt(coordinateParent.position.x)}, {Mathf.RoundToInt(coordinateParent.position.z)}]";
+        
+    }
+
+    void Update()
+    {
+        if (_coordinateParent == null) return;
+        _rectTransform.position = Camera.main.WorldToScreenPoint(_coordinateParent.position);
+    }
+
+    public void Hide()
+    {
+        _rectTransform.anchoredPosition = _offScreenAnchoredPosition;
+        _coordinateParent = null;
+    }
+
+    
+     
+}
